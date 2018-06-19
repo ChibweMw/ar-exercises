7e8c7e28503e48b1f9364f2f3b02e14ac326ea4c
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-19 20:18:44 +0000

    renamed 'Burnaby' store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,7 +9,11 @@ puts "----------"
 Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-puts Store.count
+# puts Store.count
 #READ
+@store1 = Store.find(1)
+@store2 = Store.find(2)
 #UPDATE
+@store1.name = 'Family Fashion'
+p @store1
 #DELETE
