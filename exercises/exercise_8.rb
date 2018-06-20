c3171036bc1c6597b7eaf5bc9531869896ed4476
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-20 01:20:34 +0000

    password creation callback implemented

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,24 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+require_relative './exercise_7'
+
+puts "Exercise 8"
+puts "----------"
+
+# puts "Create a new employee!"
+# puts "-------------------"
+# puts "Enter first name"
+# firstName = gets.chomp
+# puts "Enter last name"
+# lastName = gets.chomp.to_i
+# puts "Enter hourly rate"
+# hourlyRate = gets.chomp
+
+newEmployee = @store1.employees.create(first_name: "Paul", last_name: "Sweenie", hourly_rate: 60, )
+
+puts newEmployee.errors.full_messages
